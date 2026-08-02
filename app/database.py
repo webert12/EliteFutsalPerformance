9ca00b640855812f4c61d621d630ac249ebb8239
webert12 <56725537+webert12@users.noreply.github.com>
2026-08-02 16:25:48 -0300

    Refactor database connection and session handling
    Refactor database connection setup and session management.

--- app/database.py
+++ app/database.py
@@ -1,9 +1,37 @@
-from app.extensions import engine
-from app.models import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+from app.config import settings
 
 
-def create_database():
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True
+)
 
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
+Base = declarative_base()
+
+
+def criar_tabelas():
     Base.metadata.create_all(
         bind=engine
     )
+
+
+def get_db():
+
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
