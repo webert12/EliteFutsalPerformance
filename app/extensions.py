b8c4524731fa774ae7c975ab601334483ac1d717
webert12 <56725537+webert12@users.noreply.github.com>
2026-08-02 16:08:22 -0300

    Set up SQLAlchemy engine and session factory

--- app/extensions.py
+++ app/extensions.py
@@ -1,1 +1,17 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
+from app.config import settings
+
+
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True
+)
+
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
