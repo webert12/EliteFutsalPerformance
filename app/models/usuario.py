57d16b5664bc52d94b87dcbbf91c099f037d708d
webert12 <56725537+webert12@users.noreply.github.com>
2026-08-02 16:27:00 -0300

    Update usuario.py

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -1,1 +1,54 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime
 
+from app.database import Base
+
+
+class Usuario(Base):
+
+    __tablename__ = "usuarios"
+
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+
+    nome = Column(
+        String(100),
+        nullable=False
+    )
+
+
+    email = Column(
+        String(150),
+        unique=True,
+        nullable=False,
+        index=True
+    )
+
+
+    senha_hash = Column(
+        String(255),
+        nullable=False
+    )
+
+
+    tipo_usuario = Column(
+        String(30),
+        default="atleta"
+    )
+
+
+    ativo = Column(
+        Boolean,
+        default=True
+    )
+
+
+    data_criacao = Column(
+        DateTime,
+        default=datetime.utcnow
+  )
