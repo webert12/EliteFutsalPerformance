bc320190fd0acdf728b0f93f687ccbe1b73de486
webert12 <56725537+webert12@users.noreply.github.com>
2026-08-02 16:31:33 -0300

    Add token creation function with expiration

--- app/security/token.py
+++ app/security/token.py
@@ -1,1 +1,33 @@
+from datetime import datetime, timedelta
 
+from jose import jwt
+
+from app.config import settings
+
+
+ALGORITMO = "HS256"
+
+
+def criar_token(dados: dict):
+
+    dados_token = dados.copy()
+
+    validade = datetime.utcnow() + timedelta(
+        hours=24
+    )
+
+    dados_token.update(
+        {
+            "exp": validade
+        }
+    )
+
+
+    token = jwt.encode(
+        dados_token,
+        settings.SECRET_KEY,
+        algorithm=ALGORITMO
+    )
+
+
+    return token
