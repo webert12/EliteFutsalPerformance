ded7142b690f58140633a4e49958c8bcc7dd4e03
webert12 <56725537+webert12@users.noreply.github.com>
2026-08-02 16:08:02 -0300

    Implement settings class with environment variables
    Added environment variable loading and settings class.

--- app/config.py
+++ app/config.py
@@ -1,1 +1,29 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+
+class Settings:
+
+    APP_NAME = os.getenv(
+        "APP_NAME",
+        "Elite Futsal Performance"
+    )
+
+    DATABASE_URL = os.getenv(
+        "DATABASE_URL"
+    )
+
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "chave-temporaria"
+    )
+
+    ENVIRONMENT = os.getenv(
+        "ENVIRONMENT",
+        "development"
+    )
+
+
+settings = Settings()
