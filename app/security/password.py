39f54c85a0f870335e797d7dd93cc5d10264a73f
webert12 <56725537+webert12@users.noreply.github.com>
2026-08-02 16:31:03 -0300

    Update password.py

--- app/security/password.py
+++ app/security/password.py
@@ -1,1 +1,23 @@
+from passlib.context import CryptContext
 
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+
+
+def criar_hash_senha(senha: str):
+
+    return pwd_context.hash(senha)
+
+
+def verificar_senha(
+    senha_digitada: str,
+    senha_hash: str
+):
+
+    return pwd_context.verify(
+        senha_digitada,
+        senha_hash
+    )
